75520372ff170a78b03fee5ff44b970d4c29ad94
Renn <diegofebrigas22@gmail.com>
2026-09-03 20:25:55 +0700

    feat: add whatsapp client package

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"wa-bot-go/internal/whatsapp"
+)
 
 func main() {
+	client := whatsapp.Client{}
+
 	fmt.Println("WhatsApp Bot Starting...")
+	fmt.Printf("Client: %+v\n", client)
 }
